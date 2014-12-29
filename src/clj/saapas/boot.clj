ab03f0ad65d14159b2251e553e1c4393ca04f976
Juho Teperi <juho.teperi@iki.fi>
2014-12-29 21:03:35 +0200

    add-js-lib task not used anymore

--- src/clj/saapas/boot.clj
+++ src/clj/saapas/boot.clj
@@ -16,19 +16,3 @@
         (go)
         (reset! done true))
       fileset)))
-
-(deftask add-js-lib
-  "Add non-boot ready js files to the fileset"
-  [p path PATH str "The path of file in classpath"
-   t target TARGET str "Target path"
-   x package bool "Don't include files in result"]
-  (let [tmp (temp-dir!)
-        state (atom nil)]
-    (with-pre-wrap
-      fileset
-      (when-not @state
-        (let [f (io/resource path)]
-          (if f
-            (pod/copy-url f (io/file tmp target))
-            (throw (str "File " path " not found!")))))
-      (-> fileset ((if package add-source add-resource) tmp) commit!))))
