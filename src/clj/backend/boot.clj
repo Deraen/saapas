92f282dc00f49efe2e2a408c3ebdffb7cd8f43fe
Juho Teperi <juho.teperi@iki.fi>
2017-01-30 20:09:26 +0200

    Close app in boot cleanup hook

--- src/clj/backend/boot.clj
+++ src/clj/backend/boot.clj
@@ -1,7 +1,7 @@
 (ns backend.boot
   {:boot/export-tasks true}
   (:require [boot.core :as b]
-            [reloaded.repl :refer [go]]
+            [reloaded.repl :refer [go stop]]
             [backend.main :refer :all]
             [clojure.tools.namespace.repl :refer [disable-reload!]]))
 
@@ -10,6 +10,7 @@
 (b/deftask start-app
   [p port   PORT int  "Port"]
   (let [x (atom nil)]
+    (b/cleanup (stop))
     (b/with-pre-wrap fileset
       (swap! x (fn [x]
                   (if x
